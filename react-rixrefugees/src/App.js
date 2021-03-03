99c6c64d32c3cc21d59fc0ba8f72816e9335f17c
GonoGoat <he201654@students.ephec.be>
2021-03-03 23:55:15 +0100

    Test final correction CI master

--- react-rixrefugees/src/App.js
+++ react-rixrefugees/src/App.js
@@ -14,7 +14,6 @@ function App() {
           <Route path="/about" exact component={() => <About />} />
           <Route path="/manage/places" exact component={() => <Places />} />
         </Switch>
-        <Footer/>
       </Router>
     </div>
   );
