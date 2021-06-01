3a5af32ef397ce804898fc17374c6730efbecd10
GonoGoat <he201654@students.ephec.be>
2021-06-01 11:31:34 +0200

    Correction SQL

--- database/values.sql
+++ database/values.sql
@@ -6,9 +6,9 @@ insert into places_availabilities (start_avail,end_avail,bed_quantity,places_id)
     values ('1977-04-22T01:00:00-05:00 ','1978-04-22T01:00:00-05:00',5,1),('2022-08-22 19:10:25 ','2022-08-22 19:20:25 ',3,2),('2025-08-22 19:10:25 ','2025-08-22 19:20:25 ',9,3);
 
 --Sessions et utilisateurs--
-insert into users (password,fname,lname,mail,isAdmin,isActive,lastActivity,contact)
-    values ('test123','Marc','Deville','marc.deville54@outlook.fr',true,true,now(),''),('test123','Gauthier','Verschraegen','gauth.vrsgn@outlook.be',false,true,now(),''),
-    ('test123','Claude','Masson','c.masson@ephec.be',true,true,now(),''),('test123','Lucas','Pastori','lucas.pastori@gmail.com',true,true,now(),'');
+insert into users (password,fname,lname,mail,isAdmin,isActive,lastActivity,contact,token,expireToken)
+    values ('test123','Marc','Deville','marc.deville54@outlook.fr',true,true,now(),'','a',current_date),('test123','Gauthier','Verschraegen','gauth.vrsgn@outlook.be',false,true,now(),'','a',current_date),
+    ('test123','Claude','Masson','c.masson@ephec.be',true,true,now(),'','a',current_date),('test123','Lucas','Pastori','lucas.pastori@gmail.com',true,true,now(),'','a',current_date);
 insert into sessions (start_date,end_date,users_id,places_availabilities_id) 
     values (now(),now(),1,1),('2050-06-12','2050-06-12',1,2),(now(),now(),3,3);
 
@@ -22,7 +22,7 @@ insert into sessions_tasks (isfromadmin,description,amountofpeople, start_date,e
 insert into status (name) values ('Demandeur'),('CGRA'),('Réfugié');
 insert into friends (fname,status_id) values ('Paul',1),('Pierre',2),('Jacque',3),('Toto',1);
 insert into appointments (appointment,iscanceled, status_id, friends_id)
-    values ('2025-08-22',false,1,2),('2025-08-22',false,2,3),('2025-08-22',false,3,1)
+    values ('2025-08-22',false,1,2),('2025-08-22',false,2,3),('2025-08-22',false,3,1);
 
 --Disponibilités et Assignations--
 insert into availabilities (description,isCanceled,users_id,sessions_tasks_id)
