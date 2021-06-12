d89118a272e671b4f78200778ef10746c43626fd
GonoGoat <he201654@students.ephec.be>
2021-06-13 00:54:47 +0200

    Début style

--- react-rixrefugees/src/Components/Accueil.jsx
+++ react-rixrefugees/src/Components/Accueil.jsx
@@ -1,27 +1,37 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid"
 
+import join from "../img/join.jpg"
+
+import classes from "../Style/Accueil";
+const useStyles=classes;
+
 function Accueil() {
+
+    const styles = useStyles();
+
     return (
         <Container>
             <Grid container>
                 <Grid item xs={12}>
                     <Grid container>
                         <Grid item xs={6}>
-                            <img src={join}/>
+                            <img className={styles.img} src={join}/>
                         </Grid>
                         <Grid item xs={6}>
-                            <Typography>C'est cool !</Typography>
+                            <Typography variant='h4'>Bienvenue chez RixRefugees !</Typography>
+                            <Typography>L'association RixRefugees s'occupe de l'accueil de personnes migrantes dans la province de Rixensart en Belgique. Nous leur proposons un hébergement ainsi que des activités d'intégration sociale. Nous effectuons également des </Typography>
                         </Grid>
                     </Grid>
                 </Grid>
                 <Grid item xs={12}>
                     <Grid container>
                         <Grid item xs={6}>
-                            <img src={join}/>
+                            <img className={styles.img} src={join}/>
                         </Grid>
                         <Grid item xs={6}>
                             <Typography>C'est cool !</Typography>
---
+++ react-rixrefugees/src/Style/Accueil.js
@@ -0,0 +1,9 @@
+const { makeStyles } = require("@material-ui/core")
+
+const classes = makeStyles((theme) => ({
+    img : {
+        width : '100%'
+    }
+}));
+
+module.exports = classes
